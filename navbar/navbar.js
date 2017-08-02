b79265796878a131f8d7abf16a821aa23373620c
lapinoux02 <paulbnt@free.fr>
2017-08-02 19:19:29 +0200

    Changement police menu

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,8 +1,11 @@
 Vue.component('navbar', {
 	template: 
-		'<div class="raw navbar">'+
-			'<div class="col-xs-1 button">Acceuil</div>'+
-			'<div class="col-xs-1 button">Albums</div>'+
-			'<div class="col-xs-1 button">Ajout</div>'+
+		'<div class="navbar">'+
+			'<div class="row">'+
+				'<div class="col-xs-2 button">Acceuil</div>'+
+				'<div class="col-xs-2 button">Albums</div>'+
+				'<div class="col-xs-2 button">Ajout</div>'+
+				'<div class="col-xs-2 col-xs-offset-4 button">Import</div>'+
+			'</div>'+
 		'</div>'
 });
