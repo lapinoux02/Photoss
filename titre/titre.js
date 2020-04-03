48d4bb255b0d52c0eb03efa749de20e3371eda8f
lapinoux02 <paulbnt@free.fr>
2020-04-03 10:45:15 +0200

    Fix chemin relatif lettres

--- titre/titre.js
+++ titre/titre.js
@@ -1,12 +1,12 @@
 Vue.component('titre', {
 	template :
 		`<div class="titre">
-			<lettre lettre-url="../ressources/images/Lettre P.png" alt="P"></lettre>
-			<lettre lettre-url="../ressources/images/Lettre H.png" alt="H"></lettre>
-			<lettre lettre-url="../ressources/images/Lettre O.png" alt="O"></lettre>
-			<lettre lettre-url="../ressources/images/Lettre T.png" alt="T"></lettre>
-			<lettre lettre-url="../ressources/images/Lettre O.png" alt="O"></lettre>
-			<lettre lettre-url="../ressources/images/Lettre S.png" alt="S"></lettre>
-			<lettre lettre-url="../ressources/images/Lettre S.png" alt="S"></lettre>
+			<lettre lettre-url="./ressources/images/Lettre P.png" alt="P"></lettre>
+			<lettre lettre-url="./ressources/images/Lettre H.png" alt="H"></lettre>
+			<lettre lettre-url="./ressources/images/Lettre O.png" alt="O"></lettre>
+			<lettre lettre-url="./ressources/images/Lettre T.png" alt="T"></lettre>
+			<lettre lettre-url="./ressources/images/Lettre O.png" alt="O"></lettre>
+			<lettre lettre-url="./ressources/images/Lettre S.png" alt="S"></lettre>
+			<lettre lettre-url="./ressources/images/Lettre S.png" alt="S"></lettre>
 		</div>`
 })
